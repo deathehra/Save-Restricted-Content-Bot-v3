935ec62cfb7eaf1baed6cf3a85070b14be03afde
deathehra <jarvidniies@gmail.com>
2025-12-23 17:22:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -20,8 +20,8 @@ YTUB_COOKIES = """
 """
 
 # ─── BOT / DATABASE CONFIG ──────────────────────────────────────────────────────
-API_ID       = os.getenv("API_ID", "")
-API_HASH     = os.getenv("API_HASH", "")
+API_ID       = os.getenv("API_ID", "29385418")
+API_HASH     = os.getenv("API_HASH", "5737577bcb32ea1aac1ac394b96c4b10")
 BOT_TOKEN    = os.getenv("BOT_TOKEN", "")
 MONGO_DB     = os.getenv("MONGO_DB", "")
 DB_NAME      = os.getenv("DB_NAME", "tg_downloader")
@@ -76,4 +76,5 @@ P0 = {
 # ════════════════════════════════════════════════════════════════════════════════
 # ░ DEVGAGAN
 # ════════════════════════════════════════════════════════════════════════════════
+
 
